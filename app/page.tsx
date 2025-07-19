45ffd7439449ac8188d63c847978fd147b44e9de
JEREMIAH NUENO <jeremiahnueno2017@gmail.com>
2025-07-19 22:40:47 +0800

    error fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 import { useState } from "react";
 
 export default function Home() {
-  let [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [message, setMessage] = useState("");
 
   const data = [
