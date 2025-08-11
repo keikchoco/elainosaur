d35ddd7d0e1cfb5c09f6df47c0cff0f695dfeec8
JEREMIAH NUENO <jeremiahnueno2017@gmail.com>
2025-08-12 03:25:32 +0800

    [FIX] Error fix

--- app/api/access_log/route.ts
+++ app/api/access_log/route.ts
@@ -4,7 +4,7 @@ import clientPromise from "../../../lib/mongodb";
 export async function POST(request: NextRequest) {
   const requestBody = await request.json();
   const client = await clientPromise;
-  const db: any = client.db("Elainosaur");
+  const db = client.db("Elainosaur");
   db.collection("access_log").insertOne({
     access_time: requestBody.access_time,
     moodClicked: requestBody.moodClicked,
